ebb77de9a77717cf3d06e6778ae0fa3ea95ffb3c
jinshenghao <1728471999@qq.com>
2022-08-13 09:13:22 +0800

    commit Home Page

---
+++ home.py
@@ -0,0 +1,228 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'integrate.ui'
+##
+## Created by: Qt User Interface Compiler version 6.3.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+## TODO 每次展示task前调用compare函数
+
+import sys
+
+from PySide6 import QtWidgets
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QListWidget, QListWidgetItem,
+                               QMainWindow, QSizePolicy, QWidget, QSpacerItem, QPushButton, QVBoxLayout, QLabel, QFrame,
+                               QRadioButton, QButtonGroup, QDateEdit)
+
+from main import MainWindow
+from modules import resources_rc, Ui_MainWindow
+
+from task import Task
+
+
+def home_ui_init(ui):
+
+    # ui.Home_centralwidget = QWidget(MainWindow)
+    # self.Home_centralwidget.setObjectName(u"Home_centralwidget")
+    ui.home.setStyleSheet(
+        u"background-color: rgb(122, 138, 202);\n"
+        "QRadioButton::indicator {\n"
+        "    border: 3px solid #6272a4;\n"
+        "	width: 15px;\n"
+        "	height: 15px;\n"
+        "	border-radius: 10px;\n"
+        "    background: #6272a4;\n"
+        "}\n"
+        "QRadioButton::indicator:hover {\n"
+        "    border: 3px solid rgb(119, 136, 187);\n"
+        "}\n"
+        "QRadioButton::indicator:checked {\n"
+        "    background: 3px solid #bd93f9;\n"
+        "	border: 3px solid #bd93f9;	\n"
+        "}\n"
+        "")
+    # ui.horizontalLayout = QHBoxLayout(ui.Home_centralwidget)
+    # ui.horizontalLayout.setObjectName(u"Home_horizontalLayout")
+    ui.Home_verticalLayout = QVBoxLayout(ui.home)
+    ui.Home_verticalLayout.setObjectName(u"Home_verticalLayout")
+    # self.Home_frame = QFrame(self.Home_centralwidget)
+    ui.Home_frame = QFrame()
+    ui.Home_frame.setObjectName(u"Home_frame")
+    sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Minimum)
+    sizePolicy.setHorizontalStretch(0)
+    sizePolicy.setVerticalStretch(1)
+    sizePolicy.setHeightForWidth(ui.Home_frame.sizePolicy().hasHeightForWidth())
+    ui.Home_frame.setSizePolicy(sizePolicy)
+    ui.Home_frame.setMaximumSize(QSize(16777215, 80))
+    ui.Home_frame.setStyleSheet(u"border-radius: 15px;\n"
+                                  "padding: 2px;\n"
+                                  "background-color:rgb(253,253,253);\n"
+                                  " ")
+    ui.Home_frame.setFrameShape(QFrame.StyledPanel)
+    ui.Home_frame.setFrameShadow(QFrame.Raised)
+    ui.Home_horizontalLayout_4 = QHBoxLayout(ui.Home_frame)
+    ui.Home_horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+    ui.Home_label = QLabel(ui.Home_frame)
+    ui.Home_label.setObjectName(u"Home_label")
+    sizePolicy1 = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+    sizePolicy1.setHorizontalStretch(0)
+    sizePolicy1.setVerticalStretch(0)
+    sizePolicy1.setHeightForWidth(ui.Home_label.sizePolicy().hasHeightForWidth())
+    ui.Home_label.setSizePolicy(sizePolicy1)
+    ui.Home_label.setMaximumSize(QSize(30, 30))
+    ui.Home_label.setStyleSheet(u"border-image: url(:/icons/images/icons/screening.png);")
+
+    ui.Home_horizontalLayout_4.addWidget(ui.Home_label)
+
+    ui.Home_horizontalSpacer_3 = QSpacerItem(20, 20, QSizePolicy.Fixed, QSizePolicy.Minimum)
+
+    ui.Home_horizontalLayout_4.addItem(ui.Home_horizontalSpacer_3)
+
+    ui.Home_verticalLayout_2 = QVBoxLayout()
+    ui.Home_verticalLayout_2.setObjectName(u"Home_verticalLayout_2")
+    ui.Home_horizontalLayout_2 = QHBoxLayout()
+    ui.Home_horizontalLayout_2.setObjectName(u"Home_horizontalLayout_2")
+    ui.All_radioButton = QRadioButton(ui.Home_frame)
+    ui.Home_buttonGroup = QButtonGroup()
+    ui.Home_buttonGroup.setObjectName(u"Home_buttonGroup")
+    ui.Home_buttonGroup.addButton(ui.All_radioButton)
+    ui.All_radioButton.setObjectName(u"All_radioButton")
+    sizePolicy2 = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
+    sizePolicy2.setHorizontalStretch(1)
+    sizePolicy2.setVerticalStretch(0)
+    sizePolicy2.setHeightForWidth(ui.All_radioButton.sizePolicy().hasHeightForWidth())
+    ui.All_radioButton.setSizePolicy(sizePolicy2)
+    ui.All_radioButton.setChecked(True)
+
+    ui.Home_horizontalLayout_2.addWidget(ui.All_radioButton)
+
+    ui.Sport_radioButton = QRadioButton(ui.Home_frame)
+    ui.Home_buttonGroup.addButton(ui.Sport_radioButton)
+    ui.Sport_radioButton.setObjectName(u"Sport_radioButton")
+    sizePolicy3 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
+    sizePolicy3.setHorizontalStretch(1)
+    sizePolicy3.setVerticalStretch(0)
+    sizePolicy3.setHeightForWidth(ui.Sport_radioButton.sizePolicy().hasHeightForWidth())
+    ui.Sport_radioButton.setSizePolicy(sizePolicy3)
+
+    ui.Home_horizontalLayout_2.addWidget(ui.Sport_radioButton)
+
+    ui.Study_radioButton = QRadioButton(ui.Home_frame)
+    ui.Home_buttonGroup.addButton(ui.Study_radioButton)
+    ui.Study_radioButton.setObjectName(u"Study_radioButton")
+    sizePolicy3.setHeightForWidth(ui.Study_radioButton.sizePolicy().hasHeightForWidth())
+    ui.Study_radioButton.setSizePolicy(sizePolicy3)
+
+    ui.Home_horizontalLayout_2.addWidget(ui.Study_radioButton)
+
+    ui.Work_radioButton = QRadioButton(ui.Home_frame)
+    ui.Home_buttonGroup.addButton(ui.Work_radioButton)
+    ui.Work_radioButton.setObjectName(u"Work_radioButton")
+    sizePolicy3.setHeightForWidth(ui.Work_radioButton.sizePolicy().hasHeightForWidth())
+    ui.Work_radioButton.setSizePolicy(sizePolicy3)
+
+    ui.Home_horizontalLayout_2.addWidget(ui.Work_radioButton)
+
+    ui.Other_radioButton = QRadioButton(ui.Home_frame)
+    ui.Home_buttonGroup.addButton(ui.Other_radioButton)
+    ui.Other_radioButton.setObjectName(u"Other_radioButton")
+    sizePolicy3.setHeightForWidth(ui.Other_radioButton.sizePolicy().hasHeightForWidth())
+    ui.Other_radioButton.setSizePolicy(sizePolicy3)
+
+    ui.Home_horizontalLayout_2.addWidget(ui.Other_radioButton)
+
+    ui.Home_verticalLayout_2.addLayout(ui.Home_horizontalLayout_2)
+
+    ui.Home_horizontalLayout_3 = QHBoxLayout()
+    ui.Home_horizontalLayout_3.setObjectName(u"Home_horizontalLayout_3")
+    ui.Home_dateEdit = QDateEdit(ui.Home_frame)
+    ui.Home_dateEdit.setObjectName(u"Home_dateEdit")
+    sizePolicy4 = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
+    sizePolicy4.setHorizontalStretch(0)
+    sizePolicy4.setVerticalStretch(0)
+    sizePolicy4.setHeightForWidth(ui.Home_dateEdit.sizePolicy().hasHeightForWidth())
+    ui.Home_dateEdit.setSizePolicy(sizePolicy4)
+    ui.Home_dateEdit.setMaximumSize(QSize(150, 16777215))
+
+    ui.Home_horizontalLayout_3.addWidget(ui.Home_dateEdit)
+
+    ui.Home_horizontalSpacer = QSpacerItem(100, 20, QSizePolicy.Fixed, QSizePolicy.Minimum)
+
+    ui.Home_horizontalLayout_3.addItem(ui.Home_horizontalSpacer)
+
+    ui.Home_pushButton = QPushButton(ui.Home_frame)
+    ui.Home_pushButton.setObjectName(u"Home_pushButton")
+    sizePolicy3.setHeightForWidth(ui.Home_pushButton.sizePolicy().hasHeightForWidth())
+    ui.Home_pushButton.setSizePolicy(sizePolicy3)
+    ui.Home_pushButton.setMaximumSize(QSize(80, 80))
+    ui.Home_pushButton.setStyleSheet(u"QPushButton {\n"
+"	color: black;\n"
+"}\n"
+"QPushButton:pressed {\n"
+"	border-style: none;\n"
+"	background-color: rgb(189, 147, 249);\n"
+"	border-radius: 5px;\n"
+"}")
+
+    ui.Home_horizontalLayout_3.addWidget(ui.Home_pushButton)
+
+    ui.Home_horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Fixed, QSizePolicy.Minimum)
+
+    ui.Home_horizontalLayout_3.addItem(ui.Home_horizontalSpacer_2)
+
+    ui.Home_pushButton_2 = QPushButton(ui.Home_frame)
+    ui.Home_pushButton_2.setObjectName(u"Home_pushButton_2")
+    sizePolicy2.setHeightForWidth(ui.Home_pushButton_2.sizePolicy().hasHeightForWidth())
+    ui.Home_pushButton_2.setSizePolicy(sizePolicy2)
+    ui.Home_pushButton_2.setMaximumSize(QSize(80, 16777215))
+    ui.Home_pushButton_2.setStyleSheet(u"QPushButton {\n"
+"	color: black;\n"
+"}\n"
+"QPushButton:pressed {\n"
+"	border-style: none;\n"
+"	background-color: rgb(189, 147, 249);\n"
+"	border-radius: 5px;\n"
+"}")
+
+    ui.Home_horizontalLayout_3.addWidget(ui.Home_pushButton_2)
+
+    ui.Home_verticalLayout_2.addLayout(ui.Home_horizontalLayout_3)
+
+    ui.Home_horizontalLayout_4.addLayout(ui.Home_verticalLayout_2)
+
+    ui.Home_verticalLayout.addWidget(ui.Home_frame)
+
+    ui.Home_listWidget = QListWidget()
+    ui.Home_listWidget.setObjectName(u"Home_listWidget")
+    sizePolicy5 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+    sizePolicy5.setHorizontalStretch(0)
+    sizePolicy5.setVerticalStretch(4)
+    sizePolicy5.setHeightForWidth(ui.Home_listWidget.sizePolicy().hasHeightForWidth())
+    ui.Home_listWidget.setSizePolicy(sizePolicy5)
+    ui.Home_listWidget.setStyleSheet(u"border:none;")
+
+    ui.Home_label.setText("")
+    ui.All_radioButton.setText("All")
+    ui.Sport_radioButton.setText("Sport")
+    ui.Study_radioButton.setText("Study")
+    ui.Work_radioButton.setText("Work")
+    ui.Other_radioButton.setText("Other")
+    ui.Home_pushButton.setText("Confirm")
+    ui.Home_pushButton_2.setText("Cancel")
+
+    ui.Home_verticalLayout.addWidget(ui.Home_listWidget)
+
+
+
+## TODO 在Main中初始化tasks
+
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from widgets import *
 from PySide6.QtCore import *
 from PySide6.QtGui import *
 from PySide6.QtWidgets import *
+from  modules import resources_rc
 
 os.environ["QT_FONT_DPI"] = "96"  # FIX Problem for High DPI and Scale above 100%
 
@@ -33,6 +34,9 @@ class MainWindow(QMainWindow):
 		global tasks
 		widgets = self.ui
 
+		from home import home_ui_init
+		home_ui_init(widgets)
+
 		# set up calendar
 		from ui_calendar import calendar_ui_init
 		calendar_ui_init(widgets)
@@ -45,6 +49,11 @@ class MainWindow(QMainWindow):
 		# ///////////////////////////////////////////////////////////////
 		Settings.ENABLE_CUSTOM_TITLE_BAR = True
 
+
+		self.ui.Home_buttonGroup.buttonClicked.connect(self.ratioButtonClick)
+		self.ui.Home_pushButton.clicked.connect(self.queryTodoWithTime)
+		self.ui.Home_pushButton_2.clicked.connect(self.cancel)
+		self.showTodo('All', None)
 		# APP NAME
 		# ///////////////////////////////////////////////////////////////
 
@@ -173,6 +182,185 @@ class MainWindow(QMainWindow):
 				print('Mouse click: LEFT CLICK')
 			if event.buttons() == Qt.RightButton:
 				print('Mouse click: RIGHT CLICK')
+
+	def finishBottomClick(self):
+		button = self.sender()
+		button = button.mapToGlobal(QPoint(0, 0)) - self.ui.Home_listWidget.mapToGlobal(QPoint(0, 0))
+		# 获取到对象
+		item = self.ui.Home_listWidget.indexAt(button)
+		row = item.row()
+		# 获取位置
+		item = self.ui.Home_listWidget.item(row)
+		widget = self.ui.Home_listWidget.itemWidget(item)
+		Id = int(widget.objectName())
+		taskMap[Id].state = 'finished'
+		# print(row)
+		# print(widget.objectName())
+		self.ui.Home_listWidget.takeItem(row)
+
+	# del item
+
+	def modifyBottomClick(self):
+		button = self.sender()
+		butt = button.mapToGlobal(QPoint(0, 0)) - self.ui.Home_listWidget.mapToGlobal(QPoint(0, 0))
+		item = self.ui.Home_listWidget.indexAt(butt)
+		widget = self.ui.Home_listWidget.itemWidget(item)
+		row = item.row()
+
+	def showTodo(self, typeString=None, date=None):
+		def struct(task):
+			centralwidget = QWidget()
+			# centralwidget.setObjectName(u"centralwidget")
+			centralwidget.setMaximumSize(QSize(16777215, 90))
+			centralwidget.setStyleSheet(u"background-color: rgb(238,239,243);\n"
+										"border-radius:10px;")
+			horizontalLayout_3 = QHBoxLayout()
+			# horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+			horizontalLayout_2 = QHBoxLayout()
+			# horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+
+			pushButton = QPushButton()
+			# pushButton.setObjectName(u"pushButton")
+			sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+			sizePolicy.setHeightForWidth(pushButton.sizePolicy().hasHeightForWidth())
+			pushButton.setSizePolicy(sizePolicy)
+			pushButton.setMinimumSize(QSize(20, 20))
+			pushButton.setMaximumSize(QSize(20, 20))
+			pushButton.setStyleSheet(u"QPushButton {\n"
+									 "	border-style: none;\n"
+									 "	border-image: url(:/icons/images/icons/c.png);\n"
+									 "}\n"
+									 "QPushButton:pressed {\n"
+									 "	border-style: none;\n"
+									 "	border-image: url(:/icons/images/icons/finish.png);\n"
+									 "}")
+			pushButton.setObjectName(task.title)
+			pushButton.clicked.connect(self.finishBottomClick)
+			horizontalLayout_2.addWidget(pushButton)
+			horizontalSpacer = QSpacerItem(10, 10, QSizePolicy.Fixed, QSizePolicy.Minimum)
+			horizontalLayout_2.addItem(horizontalSpacer)
+
+			verticalLayout = QVBoxLayout()
+			# self.verticalLayout.setObjectName(u"verticalLayout")
+			label = QLabel()
+			# self.label.setObjectName(u"label")
+			label.setText(task.title)
+			verticalLayout.addWidget(label)
+
+			horizontalLayout = QHBoxLayout()
+			# horizontalLayout.setObjectName(u"horizontalLayout")
+			label_2 = QLabel()
+			# self.label_2.setObjectName(u"label_2")
+			label_2.setText(task.ddl)
+			horizontalLayout.addWidget(label_2)
+
+			label_3 = QLabel()
+			# label_3.setObjectName(u"label_3")
+			label_3.setText(str(task.importance))
+			horizontalLayout.addWidget(label_3)
+
+			label_4 = QLabel()
+			# self.label_4.setObjectName(u"label_4")
+			label_4.setText(task.state)
+			horizontalLayout.addWidget(label_4)
+
+			verticalLayout.addLayout(horizontalLayout)
+
+			horizontalLayout_2.addLayout(verticalLayout)
+
+			obNa = task.title + "_2"
+			pushButton_2 = QPushButton()
+			pushButton_2.setObjectName(obNa)
+			sizePolicy1 = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+			sizePolicy1.setHorizontalStretch(0)
+			sizePolicy1.setVerticalStretch(0)
+			sizePolicy1.setHeightForWidth(pushButton_2.sizePolicy().hasHeightForWidth())
+			pushButton_2.setSizePolicy(sizePolicy1)
+			pushButton_2.setMinimumSize(QSize(25, 25))
+			pushButton_2.setMaximumSize(QSize(20, 20))
+			pushButton_2.setStyleSheet(u"QPushButton {\n"
+									   "	border-style: none;\n"
+									   "	border-image: url(:/icons/images/icons/modify.png);\n"
+									   "}\n"
+									   "QPushButton:pressed {\n"
+									   "	border-style: none;\n"
+									   "	background-color: rgb(189, 147, 249);\n"
+									   "	border-image: url(:/icons/images/icons/modify.png);\n"
+									   "}")
+
+			pushButton_2.clicked.connect(self.modifyBottomClick)
+			horizontalLayout_2.addWidget(pushButton_2)
+
+			horizontalLayout_3.addLayout(horizontalLayout_2)
+
+			centralwidget.setLayout(horizontalLayout_3)
+			return centralwidget
+
+		self.ui.Home_listWidget.clear()
+		if typeString is not None:
+			for pTask in taskTemp:
+				if pTask.state != 'finished':
+					if typeString == 'All' or typeString == pTask.type:
+						item = QListWidgetItem()
+						item.setSizeHint(QSize(200, 50))
+						widget = struct(pTask)
+						widget.setObjectName(str(pTask.id))
+						self.ui.Home_listWidget.addItem(item)
+						self.ui.Home_listWidget.setItemWidget(item, widget)
+		else:
+			for pTask in taskTemp:
+				if pTask.state != 'finished':
+					if date == pTask.ddl:
+						item = QListWidgetItem()
+						item.setSizeHint(QSize(200, 50))
+						widget = struct(pTask)
+						widget.setObjectName(str(pTask.id))
+						self.ui.Home_listWidget.addItem(item)
+						self.ui.Home_listWidget.setItemWidget(item, widget)
+
+	def ratioButtonClick(self):
+		btn = self.ui.Home_buttonGroup.checkedButton()
+		typeStr = btn.text()
+		if typeStr == 'All':
+			self.showTodo('All', None)
+		elif typeStr == 'Sport':
+			self.showTodo('Sport', None)
+		elif typeStr == 'Study':
+			self.showTodo('Study', None)
+		elif typeStr == 'Work':
+			self.showTodo('Work', None)
+		elif typeStr == 'Other':
+			self.showTodo('Other', None)
+
+	def queryTodoWithTime(self):
+		year = self.ui.Home_dateEdit.date().year()
+		month = self.ui.Home_dateEdit.date().month()
+		day = self.ui.Home_dateEdit.date().day()
+		riqi = str(year) + '/' + str(month) + '/' + str(day)
+		self.showTodo(None, riqi)
+
+	def cancel(self):
+		self.showTodo('All', None)
+
+
+from task import Task
+
+taskTemp = []
+taskMap = {}
+for i in range(1, 21):
+	string = "第%d个TODO" % i
+	if i < 6:
+		newTask = Task(string, "random", "jsh", "2022/8/2", "no more", 10, False, "Study", "2022/8/2", "未完成")
+	elif i < 11:
+		newTask = Task(string, "random", "jsh", "2022/8/5", "no more", 10, False, "Sport", "2022/8/5", "未完成")
+	elif i < 16:
+		newTask = Task(string, "random", "jsh", "2022/8/2", "no more", 10, False, "Work", "2022/8/2", "未完成")
+	elif i < 21:
+		newTask = Task(string, "random", "jsh", "2022/8/10", "no more", 10, False, "Other", "2022/8/10", "未完成")
+
+	newTask.id = i
+	taskTemp.append(newTask)
+	taskMap[i] = newTask
 
 
 if __name__ == "__main__":
